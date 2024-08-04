59ccc58819dd27c2f232a217b14a95149cf495a1
marat0n <marat2255a@gmail.com>
2024-08-04 04:36:55 +0300

    add + fix: changing the common tests for yer! macro

--- tests/common.rs
+++ tests/common.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod tests {
     use std::i32;
-    use std::str;
 
     use yerevan::yer;
 
@@ -17,11 +16,20 @@ mod tests {
         pub fn ret<T>(val: T) -> Option<T> {
             Some(val)
         }
-        pub fn lazy<T>(val: T) -> Option<T> {
-            Some(val)
-        }
         pub fn zero<T>() -> Option<T> {
             None
+        }
+        pub fn ret_yield<T>(val: T) -> Option<Vec<T>> {
+            Some(vec![val])
+        }
+        pub fn combine<T>(val1: Option<Vec<T>>, val2: Option<Vec<T>>) -> Option<Vec<T>> {
+            match (val1, val2) {
+                (Some(mut unwrapped1), Some(mut unwrapped2)) => {
+                    unwrapped2.append(&mut unwrapped1);
+                    Some(unwrapped2)
+                },
+                _ => None
+            }
         }
     }
 
@@ -35,31 +43,122 @@ mod tests {
         }
     }
 
-    pub fn showcase(wrapped1: Option<i32>, wrapper2: Option<&str>, for_do: Option<()>) -> Option<i32> {
-        yer!(
+
+    #[test]
+    fn check_binding_for_simplebinder() {
+        let value_from_yer_macro = yer!(
             SimpleBinder =>
-            let! v = wrapped1;
-            let a = 1;
-            let! w = wrapper2;
-            do! for_do;
+            let! unwrapper1 = Some(1);
+            let! unwrapped2 = Some(2);
+            ret unwrapped2 + unwrapper1
+        );
+
+        assert_eq!(
+            value_from_yer_macro,
+            SimpleBinder::bind(Some(1), &|unwrapped1| {
+                SimpleBinder::bind(Some(2), &|unwrapped2| {
+                    SimpleBinder::ret(unwrapped2 + unwrapped1)
+                })
+            }),
+            "Testing macro is returning the same value as the same non-yerevanized expression"
+        );
+
+        assert_eq!(
+            value_from_yer_macro,
+            Some(3),
+            "Testing specific tested yer-macro is returning Some(3) from the sum of binded Some(1) and binded Some(2)"
+        );
+    }
+
+    #[test]
+    fn check_binding_for_incrementer() {
+        let value_from_yer_macro = yer!(
             Incrementer =>
-            let! x = a + v + (w.len() as i32);
-            ret x
-        )
+            let! unwrapper1 = 1; // incrementing to 2
+            let! unwrapped2 = 2; // incrementing to 3
+            ret unwrapped2 + unwrapper1 // returning 5
+        );
+
+        assert_eq!(
+            value_from_yer_macro.clone(),
+            Incrementer::bind(1, &|unwrapped1| {
+                Incrementer::bind(2, &|unwrapped2| {
+                    Incrementer::ret(unwrapped2 + unwrapped1)
+                })
+            }),
+            "Testing macro is returning the same value as the same non-yerevanized expression"
+        );
+
+        assert_eq!(
+            value_from_yer_macro, 5,
+            "Testing specific tested yer-macro is returning 5 from the sum of binded 1 and binded 2"
+        );
     }
 
     #[test]
-    fn check() {
-        assert_eq!(showcase(Some(5), Some(&"something"), Some(())), Some(16));
+    fn check_zero() {
+        assert_eq!(
+            yer!(
+                SimpleBinder =>
+                do! None::<()>;
+                ret ()
+            ),
+            None,
+            "Testing not successful returning"
+        );
     }
 
     #[test]
-    fn check2() {
-        assert_eq!(showcase(Some(1), None, Some(())), None);
+    fn check_yielding() {
+        let value_from_yer_macro = yer!(
+            SimpleBinder =>
+            let! some_value = Some(1);
+            yield some_value; // yielding 1
+            yield 2; // yielding 2
+            let some_sum = some_value + 2; // result is 3
+            yield some_sum; // yielding 3
+        );
+
+        assert_eq!(
+            value_from_yer_macro,
+            Some(vec![1, 2, 3]),
+            "Testing yielding values to one-dimensional vec"
+        );
     }
 
     #[test]
-    fn check3() {
-        assert_eq!(showcase(Some(1), Some(&"something"), None), None);
+    fn check_ce_nesting() {
+        let value_from_yer_macro = yer!(
+            SimpleBinder =>
+            let! unwrapped1 = Some(2); // 2
+            let! unwrapped2 = Some(3); // 3
+            let one = 1;
+            Incrementer =>
+            let! res = one + unwrapped1 + unwrapped2; // incremented by 1 result of 1 + 2 + 3 = 6 + 1 = 7
+            ret res // Some(7)
+        );
+
+        assert_eq!(
+            value_from_yer_macro, 
+            SimpleBinder::bind(
+                Some(2), &|unwrapped1| {
+                    SimpleBinder::bind(
+                        Some(3), &|unwrapped2| {
+                            let one = 1;
+                            SimpleBinder::ret(
+                                Incrementer::bind(
+                                    one + unwrapped1 + unwrapped2, &|res| {
+                                        Incrementer::ret(res)
+                                    })
+                            )
+                        })
+                }),
+            "Testing macro is returning the same value as the same non-yerevanized expression"
+        );
+
+        assert_eq!(
+            value_from_yer_macro, Some(7),
+            "Testing specific tested yer-macro is returning a correct result"
+        );
     }
 }
