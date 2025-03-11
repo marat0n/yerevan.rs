f8ae4459323f99fb6b114e2aced4f10c782ff02c
marat0n <marat2255a@gmail.com>
2025-03-11 11:29:15 +0400

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 //! # yerevan.rs
-//! yerevan.rs is a package providing functionality and syntax of computation expressions (CEs)
-//! from F#, but with several changes and improvements.
+//! yerevan.rs is a PoC package where I experiment with adding functionality and syntax of Computation Expressions (CEs)
+//! from F#, aka do-notations from Haskell, but with several changes and improvements.
 //!
 //! Main component of yerevan.rs package is the [`yer!`] macro.
 //!
 //! ## Note
-//! The things called [computation expressions](https://fsharpforfunandprofit.com/posts/computation-expressions-intro/) from F# are the same as [do-notations](https://en.wikibooks.org/wiki/Haskell/do_notation) from Haskell.
+//! The things called [computation expressions](https://fsharpforfunandprofit.com/posts/computation-expressions-intro/) from F# are almost the same as [do-notations](https://en.wikibooks.org/wiki/Haskell/do_notation) from Haskell.
 //!
 //! ## Features and syntax of [`yer!`] macro:
 //! |Feature|Syntax|F# way|
@@ -22,10 +22,10 @@
 //!
 //! ## How to implement these methods in your struct?
 //! ### Note to Methods API
-//! There is no specific types you have to use by using methods signatures defined below.
+//! There is no specific type you have to use by using methods signatures defined below.
 //!
 //! The [yer!] macro is very flexible, so you must only use your types in a right way knowing
-//! how CEs are work inside.
+//! how CEs work inside.
 //!
 //! Also check the examples in [yer!]-page and tests in [tests/common.rs file in the repo](https://github.com/marat0n/yerevan.rs/blob/dev/tests/common.rs) to better understand how CEs work inside.
 //! ### Methods API
@@ -79,10 +79,10 @@
 //! The ? operator is based on the type you return from the function which makes it useless in cases when you need to unwrap a different type. And some packages create their own implementations of Result or Option types which also makes this operator less useful.
 //!
 //! Also in Rust you can define your own macros to extend the language possibilities and use it just like CEs but with your own syntax.
-//! But creating a big macros can be tricky and difficult. And if you created your own syntax it's doesn't mean that this syntax will be readable for you or anyone else.
+//! But creating a big macros can be tricky and difficult. And if you created your own syntax it doesn't mean that this syntax will be readable for you or anyone else.
 //!
 //! CEs are just functions you call in syntax-sugared way. So you can create your own custom control-flow, just like with macros but with standard syntax.
-//! For example in CE-functions you can implement: builder pattern, safe unwrapper of enums (like Option or Result), etc.
+//! For example, in CE-functions you can implement: builder pattern, safe unwrapper of enums (like Option or Result), etc.
 
 #[macro_use]
 pub mod yer_macro;
